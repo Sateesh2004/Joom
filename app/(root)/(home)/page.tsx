dd74e5bf4b5f1666858bc5e95b5095d5bd7c1a24
Sateesh <kumarsateesh838@gmail.com>
2024-04-06 03:37:59 +0530

    Capitalized

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,8 +4,8 @@ const Home = () => {
   const now = new Date();
 
   // const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' })+1;
-  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now)+1;
-
+  // const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+  
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
@@ -14,8 +14,8 @@ const Home = () => {
             Upcoming Meeting at: 12:00 PM
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">Welcome to Joom</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">Welcome to JOOM</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">Experience Seamless Collaboration with Our JOOM</p>
           </div>
         </div>
       </div>
